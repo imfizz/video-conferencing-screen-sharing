4515503b6b3e984c9f1ea93b93953a1c72b3f24c
Francis Albert Ilacad <filacad22@gmail.com>
2021-10-28 16:16:57 +0800

    sample video

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const server = require('http').Server(app) // socket.io
+const io = require('socket.io')(server) // socket.io
 
-app.set('view engine', 'ejs');
+const { v4: uuidv4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public')) // will server as a static file
 
 app.get('/', (req, res) => {
-    res.render('main.ejs');
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:roomId', (req, res) => {
+    res.render('main', { roomId: req.params.roomId }) // redirect to main.ejs file
 })
 
 
 
-app.listen(3000);
+
+
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).emit('user-connected', userId);
+        socket.on('disconnect', () => {
+            socket.to(roomId).emit('user-disconnected', userId);
+        })
+    })
+})
+
+server.listen(3000);
---
+++ public/script2.js
@@ -0,0 +1,83 @@
+const socket = io('/');
+
+const peer = new Peer(undefined, {
+    host: '/',
+    port: '3000'
+});
+
+const myVideo = document.querySelector('#myVideo');
+const peers = {} // dito iistore yung userId ng nagleave
+
+peer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
+});
+
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    const video = document.createElement('video');
+    addVideoStream(video, stream);
+
+
+    // ito yung final step, although nakaconnect tayo sa ibang user di parin natin makikita yung 
+    // video nila, kasi need pa natin makinig sa .answer() na method
+    // when someone tries to call us
+    peer.on('call', call => {  
+        // sagutin mo yung tawag tapos isend mo sakanila yung stream
+        call.answer(stream);
+
+        const video = document.createElement('video');
+        // sinasagot natin yung call, pero di natin hinahayaan tanggapin yung ibang video stream
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
+        })
+    })
+
+
+
+    // so when new user connects to our room, we will send them our current video stream
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream);
+    });
+
+    
+})
+
+// sa pamamagitan neto nalalaman natin yung userId na nagleave kaya pwede natin tanggalin yung vide nilang naiwan
+socket.on('user-disconnected', userId => {
+    // bilang may laman na yung peers malalaman na natin kung sino ron yung id na nagleave
+    // if existing yung id na yun sa peers
+    if(peers[userId]) peers[userId].close(); // icoclose natin yung connection nung same userId
+});
+
+
+// get the current video stream of creator
+function connectToNewUser(userId, stream){
+    const call = peer.call(userId, stream); // we will call to send them our video stream
+    const video = document.createElement('video');
+    // when they send back their video stream, stream ang tawag don
+    call.on('stream', userVideoStream => {
+        // so ngayon, yung video na sinend nila gawan mo ng video tag
+        addVideoStream(video, userVideoStream);
+    });
+
+    // pag tinapos yung tawag, burahin mo lang yung video
+    call.on('close', () => {
+        video.remove();
+    });
+
+    // every user id ay didirektang pasok sa peers na object na ginawa natin
+    peers[userId] = call;
+
+}
+
+// creation of video
+function addVideoStream(video, stream){
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    });
+    myVideo.append(video);
+}
